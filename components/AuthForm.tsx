0e4532b8ae4d8ac841c26f9dc95a229fb0320008
Karan Panchal <karanhpanchal09@gmail.com>
2025-04-17 00:45:13 +0530

    auth issue fixed

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -9,7 +9,9 @@ import { toast } from "sonner";
 import FormField from "./FormField";
 import { useRouter } from "next/navigation";
 import { Form } from "./ui/form";
-
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/firebase/client";
+import { signIn, signUp } from "@/lib/actions/auth.action";
 
 const authFormSchema = (type: FormType) => {
   return z.object({
@@ -34,14 +36,49 @@ const AuthForm = ({ type }: { type: FormType }) => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
       if (type === "sign-up") {
-        toast.success('Account created successfully. Please sign in.')
-        router.push('/sign-in')
+        const { name, email, password } = values;
+        const userCredentials = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        const result = await signUp({
+          uid: userCredentials.user.uid,
+          name: name!,
+          email,
+          password,
+        });
+
+        if (!result?.success) {
+          toast.error(result?.message);
+          return;
+        }
+
+        toast.success("Account created successfully. Please sign in.");
+        router.push("/sign-in");
       } else {
-        toast.success('Sign in successfully.')
-        router.push('/')
+
+        const {email,password} = values;
+
+        const userCredential = await signInWithEmailAndPassword(auth,email,password);
+
+        const idToken = await userCredential.user.getIdToken();
+
+        if(!idToken){
+          toast.error('Sign in failed')
+          return;
+        }
+
+        await signIn({
+          email,idToken
+        });
+
+        toast.success("Sign in successfully.");
+        router.push("/");
       }
     } catch (error) {
       console.log(error);
@@ -56,7 +93,7 @@ const AuthForm = ({ type }: { type: FormType }) => {
       <div className="flex flex-col gap-6 card py-14 px-10">
         <div className="flex flex-row gap-2 justify-center">
           <Image src="/logo.svg" alt="logo" height={32} width={38} />
-          <h2 className="text-primary-100">Prepnest</h2>
+          <h2 className="text-primary-100">Interviewly</h2>
         </div>
         <h3>Practice job interview with AI</h3>
 
