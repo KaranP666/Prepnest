5c5b300b8cb3b3adb25433ffad3e832a4ce903d7
Karan Panchal <karanhpanchal09@gmail.com>
2025-04-23 16:09:40 +0530

    Protected files

--- firebase/client.ts
+++ firebase/client.ts
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAO5ovO5lh4zCf4VAe6NxM3SYcxnHUHP74",
-  authDomain: "prepnest-2175c.firebaseapp.com",
-  projectId: "prepnest-2175c",
-  storageBucket: "prepnest-2175c.firebasestorage.app",
-  messagingSenderId: "140664170307",
-  appId: "1:140664170307:web:4095bec0f1ae44430071f6",
-  measurementId: "G-RB31JEWPY1"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
