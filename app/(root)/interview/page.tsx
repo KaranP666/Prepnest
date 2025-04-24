a46ddba7787285ab5dc57b2d68b4a3e8240efdfd
Karan Panchal <karanhpanchal09@gmail.com>
2025-04-24 15:52:48 +0530

    Feedback page done

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,6 +1,5 @@
 import Agent from '@/components/Agent'
 import { getCurrentUser } from '@/lib/actions/auth.action'
-import React from 'react'
 
 const page = async() => {
  const user = await getCurrentUser();
@@ -10,7 +9,7 @@ const page = async() => {
     <>
     <h3>Interview Generation</h3>
     {/* job of agent is to take the info from user to know what kind of interview are we planning to generate */}
-    <Agent userName={user?.name} userId={user?.id}  type="generate" />
+    <Agent userName={user?.name || ''} userId={user?.id}  type="generate" />
     </>
   )
 }
--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   eslint: {
-    ignoreDuringBuilds: true
+    ignoreDuringBuilds: true,
   },
   typescript: {
-    ignoreBuildErrors: true
-  }
+    ignoreBuildErrors: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        net: false,
+        tls: false,
+        fs: false,
+        os: false,
+        child_process: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
